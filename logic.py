860dd04c45b2d6201b140c39ea33b9c2f0b9fcbd
alexandr.demin <hseoanalytics@gmail.com>
2019-08-26 01:12:52 +0300

    Парсинг b2b-center

--- logic.py
+++ logic.py
@@ -12,6 +12,7 @@ import codecs
 import urllib.parse as urllib
 from datetime import datetime, timedelta
 import sys
+import time
 
 # Получение формата даты
 def getDateFormat(with_time = False):
@@ -101,7 +102,7 @@ def generateHtml(data, subscription, tender_types_enum):
         for t_t in fltr.get('tenderTypes'):
             name = 'Неизвестно'
             for t_e in tender_types_enum:
-                if t_t == t_e['id']:
+                if str(t_t) == str(t_e['id']):
                     name = t_e['name']
             tender_types_array.append(name)
         tender_types = ', '.join(tender_types_array)
@@ -114,29 +115,55 @@ def generateHtml(data, subscription, tender_types_enum):
             html += f'<li>Организатор: Любой</li>'
             html += f'<li>Заказчик: {fltr.get("organizerOrCustomer").get("name")}</li>'
     html += '</ul>'
-    html += '''
+    hasStuff = {}
+    for item in data:
+        if item.get('number'):
+            hasStuff['hasNumber'] = True
+        if item.get('linkText'):
+            hasStuff['hasLinkText'] = True
+        if item.get('price'):
+            hasStuff['hasPrice'] = True
+        if item.get('type'):
+            hasStuff['hasType'] = True
+        if item.get('organizer'):
+            hasStuff['hasOrganizer'] = True
+        if item.get('customer'):
+            hasStuff['hasCustomer'] = True
+    number_header = '<th>Номер закупки</th>' if hasStuff.get("hasNumber") else ''
+    link_header = '<th>Ссылка</th>' if hasStuff.get("hasLinkText") else ''
+    price_header = '<th>Цена</th>' if hasStuff.get("hasPrice") else ''
+    type_header = '<th>Тип</th>' if hasStuff.get("hasType") else ''
+    organizer_header = '<th>Организатор</th>' if hasStuff.get("hasOrganizer") else ''
+    customer_header = '<th>Заказчик</th>' if hasStuff.get("hasCustomer") else ''
+    html += f'''
         <table>
             <thead>
                 <tr>
-                    <th>Номер закупки</th>
-                    <th>Ссылка</th>
-                    <th>Цена</th>
-                    <th>Тип</th>
-                    <th>Организатор</th>
-                    <th>Заказчик</th>
+                    {number_header}
+                    {link_header}
+                    {price_header}
+                    {type_header}
+                    {organizer_header}
+                    {customer_header}
                 </tr>
             </thead>
             <tbody>
     '''
     for item in data:
+        number = f'<td>{item.get("number")}</td>' if item.get("number") else ''
+        link = f'<td><a href="{item.get("linkHref")}">{item.get("linkText")}</a></td>' if item.get("linkText") else ''
+        price = f'<td>{item.get("price")}</td>' if item.get("price") else ''
+        typ = f'<td>{item.get("type")}</td>' if item.get("type") else ''
+        organizer = f'<td>{item.get("organizer")}</td>' if item.get("organizer") else ''
+        customer = f'<td>{item.get("customer")}</td>' if item.get("customer") else ''
         html += f'''
             <tr>
-                <td>{item.get("number")}</td>
-                <td><a href="{item.get("linkHref")}">{item.get("linkText")}</a></td>
-                <td>{item.get("price")}</td>
-                <td>{item.get("type")}</td>
-                <td>{item.get("organizer")}</td>
-                <td>{item.get("customer")}</td>
+                {number}
+                {link}
+                {price}
+                {typ}
+                {organizer}
+                {customer}
             </tr>
         '''
     html += '</tbody></table>'
@@ -261,13 +288,109 @@ def parseFabrikant(config, fltr, driver, subscription):
                 page_num += 1
     return data
 
-# Парсинг b2b-center.ru
-def parseB2bCenter(config, fltr, driver, subscription):
-    driver.get(config['url'])
-
 # Парсинг roseltorg.ru
 def parseRoseltorg(config, fltr, driver, subscription):
     driver.get(config['url'])
+
+# Парсинг b2b-center.ru
+def parseB2bCenter(config, fltr, driver, subscription):
+    # Заходим на основной урл, чтобы получить куки и сессию
+    driver.get(config['url'])
+    # Принимаем куки
+    try:
+        cookie_btn = driver.find_element(config['locators']['COOKIE_BUTTON']['by'], config['locators']['COOKIE_BUTTON']['selector'])
+        cookie_btn.click()
+    except:
+        pass
+    if 'captcha' in driver.current_url:
+        writeLog({'time': datetime.now().strftime(getDateFormat(True)), 'error': f'Поймали капчу по подписке {subscription["name"]} на сайте {subscription["website"]}'})
+        return []
+    # Ждём, чтобы не забанили
+    time.sleep(1500)
+    # Генерируем урл с фильтрами
+    url_params = config['values']['URL_PARAMS']
+    if fltr.get('daysBackPublished') and fltr.get('daysBackPublished') > 0:
+        url_params['date_start_dmy'] = (datetime.today() - timedelta(days=fltr.get('daysBackPublished'))).strftime(getDateFormat())
+        url_params['date_end_dmy'] = datetime.today().strftime(getDateFormat())
+    else:
+        url_params['date_start_dmy'] = datetime.today()
+        url_params['date_end_dmy'] = datetime.today()
+    regions_params = []
+    if fltr.get('regions') and len(fltr.get('regions')) > 0:
+        i = 0
+        region_param_name = config['values']['REGIONS_PARAM_NAME']
+        for region in fltr.get('regions'):
+            url_params[region_param_name.format(i)] = region
+            i += 1
+    if fltr.get('minPrice') and fltr.get('minPrice') > 0:
+        url_params['price_start'] = fltr.get('minPrice')
+    if fltr.get('maxPrice') and fltr.get('maxPrice') > 0:
+        url_params['price_end'] = fltr.get('maxPrice')
+    if fltr.get('maxPrice') and fltr.get('minPrice') and  fltr.get('maxPrice') < fltr.get('minPrice'):
+        writeLog({'time': datetime.now().strftime(getDateFormat(True)), 'error': f'Минимальная цена {fltr.get("minPrice")} больше максимальной {fltr.get("maxPrice")} по подписке {subscription["name"]}'}, True)
+        return []
+    if fltr.get('organizerOrCustomer') and fltr.get('organizerOrCustomer').get('name') and fltr.get('organizerOrCustomer').get('type') and len(fltr.get('organizerOrCustomer')['name']) > 0:
+        org_name = fltr.get('organizerOrCustomer')['name']
+        constant_name = 'ORGANIZER' if fltr.get('organizerOrCustomer')['type'] == 'organizer' else 'CUSTOMER'
+        org_type = config['values'][constant_name]
+        org_id = driver.execute_script(config['values']['SCRIPT'].format(org_type, org_name))
+        id_param = config['values'][constant_name + '_PARAMS']['id']
+        company_type_param_value = config['values'][constant_name + '_PARAMS']['companyType']
+        url_params[id_param] = org_id
+        url_params['company_type'] = company_type_param_value
+    
+    # Перебираем кейворды, типы тендеров, парсим каждый, сохраняем информацию в общий массив
+    data = []
+    if fltr.get('tenderTypes') and len(fltr.get('tenderTypes')) > 0:
+        tender_types = fltr.get('tenderTypes')
+    else:
+        tender_types = ["0"]
+    for tender_type in tender_types:
+        url_params['lot_type'] = tender_type
+        for query in fltr['keywords']:
+            time.sleep(5)
+            page_num = 0
+            while True:
+                url_params['f_keyword'] = query
+                url_params['from'] = page_num
+                url = config['values']['URL_WITH_FILTERS'] + urllib.urlencode(url_params)
+                driver.get(url)
+                items = driver.find_elements(config['locators']['ITEM']['by'], config['locators']['ITEM']['selector'])
+                if len(items) > 0:
+                    for item in items:
+                        try:
+                            link = item.find_element(config['locators']['ITEM_LINK']['by'], config['locators']['ITEM_LINK']['selector'])
+                            number = link.text
+                            link_href = link.get_attribute('href')
+                        except:
+                            number = None
+                            link_href = None
+                        try:
+                            organizer = item.find_element(config['locators']['ITEM_ORGANIZER']['by'], config['locators']['ITEM_ORGANIZER']['selector']).text
+                        except:
+                            organizer = None
+                        try:
+                            link_text = item.find_element(config['locators']['ITEM_DESCRIPTION']['by'], config['locators']['ITEM_DESCRIPTION']['selector']).text
+                        except:
+                            link_text = None
+                        data.append({
+                            'linkText': link_text,
+                            'linkHref': link_href,
+                            'number': number.replace(link_text, '').split(' № ')[1].strip(),
+                            'type': number.replace(link_text, '').split(' № ')[0],
+                            'organizer': organizer
+                        })
+                try:
+                    last_page = int(driver.find_element(config['locators']['LAST_PAGE']['by'], config['locators']['LAST_PAGE']['selector']).text)*20
+                except:
+                    last_page = 0
+                if page_num+20 >= last_page:
+                    break
+                else:
+                    page_num += 20
+                    # Ждём, чтобы не забанили
+                    time.sleep(3+page_num/20)
+    return data
 
 # Парсинг сайтов
 def parseSite(website, parser_config, fltr, driver, subscription):
@@ -277,3 +400,5 @@ def parseSite(website, parser_config, fltr, driver, subscription):
         return parseB2bCenter(parser_config, fltr, driver, subscription)
     if website == 'roseltorg':
         return parseRoseltorg(parser_config, fltr, driver, subscription)
+
+
